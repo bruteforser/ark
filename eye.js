9d1a7784f7d827ed8b0c1ae168ed8557661207cb
archishoyo <rb5558645@gmail.com>
2026-02-07 22:07:49 -0400

    add eye and trying sclera

--- eye.js
+++ eye.js
@@ -11,3 +11,19 @@ document.addEventListener("focus", handler, true);
 document.addEventListener("blur", handler, true);
 document.addEventListener("pointerdown", handler, true);
 document.addEventListener("pointerup", handler, true);
+
+
+document.addEventListener('mousemove', (event) => {
+  const eye = document.querySelector('.eye');
+  const pupil = document.querySelector('.pupil');
+  const eyeRect = eye.getBoundingClientRect();
+  const eyeCenterX = eyeRect.left + eyeRect.width / 2;
+  const eyeCenterY = eyeRect.top + eyeRect.height / 2;
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  const angleRad = Math.atan2(mouseY - eyeCenterY, mouseX - eyeCenterX);
+  const moveDistance = 25; 
+  const pupilX = Math.cos(angleRad) * moveDistance;
+  const pupilY = Math.sin(angleRad) * moveDistance;
+  pupil.style.transform = `translate(-50%, -50%) translate(${pupilX}px, ${pupilY}px)`;
+});
